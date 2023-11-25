159343c5f52455cc45f5ae67c57718f93bb4b646
dBug23 <chibukoesta@gmail.com>
2023-11-25 18:44:53 +0100

    some major work to all three docs...changed some id and also added more code to js file

--- converter.js
+++ converter.js
@@ -1,28 +1,26 @@
-let usdAmount = document.getElementById("from-input");
-let rsdAmount = document.getElementById("to-input");
-let convertBtn = document.getElementById("convert");
+let usdAmount = document.getElementById("usdAmount");
+let rsdAmount = document.getElementById("rsdAmount");
+let convertBtn = document.getElementById("convertBtn");
 
 
 function Converter(usd,rsd){
-    if(rsd === "0" ){
-       usd = usd * 116.4;
+    if(rsd === "0" || !rsd){
+       rsd = usd * 116.4;
     };
-    if(usd === "0"){
-        rsd = rsd / 117.4;
+    if(usd === "0" || !usd){
+        usd = rsd / 117.4;
     };
 
+    usdAmount.value = usd;
+    rsdAmount.value = rsd;
     
-}
+};
 
 convertBtn.addEventListener("click", function(){
-    if(rsdAmount.value === null){
+    if(!rsdAmount.value){
         Converter(usdAmount.value, 0);
-
-
-    }else if(usdAmount.value === null){
-        Converter(0 ,rsdAmount.value);
-
-
+    }else if(!usdAmount.value){
+        Converter(0, rsdAmount.value);
     };
 
 
